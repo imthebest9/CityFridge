7a8d705dee9444f03efc7d42c002bba1ed0cfc22
WongChongYang <chongyang@student.usm.my>
2021-11-27 17:51:51 +0800

    message

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Test1</Text>
-      <Text>Yo whats up</Text>
+      <Text>Yodf</Text>
       <StatusBar style="auto" />
     </View>
   );
