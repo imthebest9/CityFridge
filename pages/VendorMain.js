fb05dfaf6d3333983b3dd3ad88e7bfaf37dc4299
imthebest9 <lookai0303@gmail.com>
2021-11-28 23:49:51 +0800

    google-font-not-working

--- pages/VendorMain.js
+++ pages/VendorMain.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import {
+  useFonts,
+  Merriweather_300Light,
+  Merriweather_300Light_Italic,
+  Merriweather_400Regular,
+  Merriweather_400Regular_Italic,
+  Merriweather_700Bold,
+  Merriweather_700Bold_Italic,
+  Merriweather_900Black,
+  Merriweather_900Black_Italic,
+} from "@expo-google-fonts/merriweather";
+import { AppLoading } from "expo-app-loading";
 
 export default function VendorMain() {
+  let [fontsLoaded] = useFonts({
+    Merriweather_300Light,
+    Merriweather_300Light_Italic,
+    Merriweather_400Regular,
+    Merriweather_400Regular_Italic,
+    Merriweather_700Bold,
+    Merriweather_700Bold_Italic,
+    Merriweather_900Black,
+    Merriweather_900Black_Italic,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
     return (
-        <View>
-            <Text>HHHHHHHHH</Text>
-        </View>
-    )
+      <View style={styles.header}>
+        <Text
+          style={{
+            fontSize: 24,
+            // fontFamily: "Merriweather_900Black",
+          }}
+        >
+          Your Store
+        </Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 30,
+  },
+});
