d0c3e74e8e40a7785eba6f0e9b6f2d1aaf05c880
zhongqquan <zhongqquan@student.usm.my>
2021-12-12 14:42:00 +0800

    msg2

--- pages/trans1.js
+++ pages/trans1.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function trans1() {
     return (
         <View>
-            <Text>hh</Text>
+            <Text>hellooo</Text>
         </View>
     )
 }
