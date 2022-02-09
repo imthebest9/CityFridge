52e8427f1a1698d8557579a1486f108f2debfc31
imthebest9 <lookai0303@gmail.com>
2022-02-09 15:05:07 +0800

    can change isComplete from false to true

--- pages/VendorConfirmOrder.js
+++ pages/VendorConfirmOrder.js
@@ -1,8 +1,40 @@
-import React from "react";
+import { collection, doc, getDocs, updateDoc } from "@firebase/firestore";
+import { useIsFocused } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import BottomTabsVendor from "../components/vendor/BottomTabsVendor";
+import { database } from "../firebase";
 
 export default function VendorConfirmOrder() {
+  const [code, setCode] = useState();
+  const [res, setRes] = useState([]);
+
+  useEffect(async () => {
+    const querySnapshot = await getDocs(collection(database, "reservations"));
+    const saveFirebaseItems = [];
+    querySnapshot.forEach((doc) => {
+      saveFirebaseItems.push(doc.data());
+    });
+    setRes(saveFirebaseItems);
+  }, [useIsFocused()]);
+
+  console.log(code);
+
+  const onConfirm = async () => {
+    for (let r in res) {
+      if (res[r].ConfirmationCode === code) {
+        try {
+          await updateDoc(doc(database, "reservations", "Reservation1"), {
+            isComplete: "True"
+          });
+          console.log("yes");
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+  };
+
   return (
     <View style={{ backgroundColor: "#fff", paddingTop: 40, flex: 1 }}>
       <View style={{ alignItems: "center", flex: 1, justifyContent: "center" }}>
@@ -11,20 +43,21 @@ export default function VendorConfirmOrder() {
         </Text>
         <TextInput
           placeholder="Verification code"
-          keyboardType="numeric"
+          // keyboardType="numeric"
+          onChangeText={(input) => setCode(input)}
           style={{
             backgroundColor: "#E8E8E8",
             margin: 20,
             height: 40,
             width: 300,
-            padding: 10
+            padding: 10,
           }}
         />
         <View
           style={{
             alignItems: "center",
             justifyContent: "center",
-            margin: 20
+            margin: 20,
           }}
         >
           <TouchableOpacity
@@ -34,6 +67,7 @@ export default function VendorConfirmOrder() {
               paddingHorizontal: 70,
               borderRadius: 30,
             }}
+            onPress={onConfirm}
           >
             <Text
               style={{
